fd10c08758f6632c46034b16971fe9250e5c00b0
ananya324 <sanodiaananya@gmail.com>
2026-01-24 01:48:30 +0530

    Update AuthPage.jsx

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -2,20 +2,20 @@ import { Link } from "react-router-dom";
 
 function AuthPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex items-center justify-center bg-purple-100">
       <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-sm text-center">
         <h1 className="text-2xl font-bold mb-6">Welcome to Notes App</h1>
 
         <Link
           to="/login"
-          className="block bg-blue-600 text-white py-2 rounded mb-3 hover:bg-blue-700"
+          className="block bg-blue-400 text-white py-2 rounded mb-3 hover:bg-blue-700"
         >
           Login
         </Link>
 
         <Link
           to="/register"
-          className="block bg-green-600 text-white py-2 rounded mb-3 hover:bg-green-700"
+          className="block bg-green-500 text-white py-2 rounded mb-3 hover:bg-green-700"
         >
           Register
         </Link>
